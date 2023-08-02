3ab5f8c9668c9c997c9c2388618c7fc8b7637ada
Cleberton F. Santos <cleber.cfsantos@gmail.com>
2023-08-01 21:53:07 -0300

    Exercicio Finalizado - Falta verificação de erro

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 //Algoritimo
 
-// 1. Pegar os valores
-// 2. Calcular a Idade
-//       a. Com base no ano
-//       b. Com mês (EXTRA)
-//       c. Com dia (EXTRA)
+// [OK] 1. Pegar os valores
+// [OK]  2. Calcular a Idade
+// [OK]        a. Com base no ano
+// [OK]        b. Com mês (EXTRA)
+// [OK]        c. Com dia (EXTRA)
 
-// 3. Gerar a faixa etária
-   
+// [OK]  3. Gerar a faixa etária
 //     Resultado            Faixa
 //     0 à 12                Criança
 //     13 à 17                Adolescente
 //     18 à 65               Adulto
 //     Acima de 65         Idoso
    
-
-// 4. Organizar o objeto pessoa para salvar na lista
-// 5. Cadastrar a pessoa na lista
+// [OK]  4. Organizar o objeto pessoa para salvar na lista
+// [OK]  5. Cadastrar a pessoa na lista
 // 6. Função para carregar as pessoas, carrega a lista do localStorage, chamar ao carregar a página
 // 7. Renderizar o conteúdo da tabela com as pessoas cadastradas
 // 8. Botão para limpar os registros;
@@ -31,11 +29,17 @@ function calcular(event){
     if(verificarErro(usuario.dia, usuario.mes, usuario.ano, usuario.nome) == false){
         return
     }
-    
-    let idadeCalculada = calcularIdade(usuario.ano);
+    //Calcula a idade com dia mes e ano
+    let idadeCalculada = calcularIdade(usuario.dia, usuario.mes, usuario.ano);
+    //recebe a classificação 
     let classificacaoIdade = classificarIdade(idadeCalculada);
-    
     console.log(classificacaoIdade)
+    //Atualiza os dados do Objeto Usuario
+    usuario = organizarDados(usuario, idadeCalculada, classificacaoIdade)
+    //Cadastra o usuario na Storage
+    cadastrarUsuario(usuario);
+    //Recarrega a página no final
+    window.location.reload();
 }
 
 
@@ -48,7 +52,6 @@ function receberValores(){
     let mesNascimento = document.getElementById("mes-nascimento").value;
     let anoNascimento = document.getElementById("ano-nascimento").value;
 
-    
     let dadosUsuario = {
         nome: nomeRecebido,
         dia: diaNascimento,
@@ -62,6 +65,7 @@ function receberValores(){
 //Passo 1.1 {VERIFICAR ERROS DE INPUTS}
 
 function verificarErro(dia_, mes_, ano_, nome_){
+    let data = new Date();
     if ((dia_ <= 0 || mes_ <= 0 || ano_ <= 0) || (dia_ == "" || mes_ == "" || ano_ == "" || nome_ == "" )){
         console.log('Dia/Mês/Ano não pode ser menor que 0 ou em branco')
         return false
@@ -75,18 +79,34 @@ function verificarErro(dia_, mes_, ano_, nome_){
         }else if(ano_ > new Date().getFullYear()){
             alert('O ano de nascimento não pode ser maior que o ano atual')
             return false
+            
+        }else if( (ano_ == data.getFullYear()) && ((mes_ > data.getMonth() /*|| (mes_ == data.getMonth() && dia_ >= data.getDate())*/)) ){
+            alert('A data de nascimento não pode ser maior que a data atual')
+            return false
         }
+        
     }
     return true;
 }
 
 //Passo 2
-function calcularIdade(anoNascimento_){
-    let anoAtual = new Date().getFullYear();
+function calcularIdade(diaNascimento_, mesNascimento_, anoNascimento_){
+    // let anoAtual = new Date().getFullYear();
+    let dataCompletaAtual = new Date();
+    let dataAtual = {
+        dia: dataCompletaAtual.getDate(),
+        mes: dataCompletaAtual.getMonth() + 1,
+        ano: dataCompletaAtual.getFullYear()
+    }
 
     // Idade = [Ano atual] - [Ano Nascimento] 
-    let idade = anoAtual - anoNascimento_;
-    console.log(anoAtual, anoNascimento_, idade)
+    let idade = dataAtual.ano - anoNascimento_;
+
+    //se o mês atual for igual/menor que o de aniversário e o dia < que o de aniversario => Ano -1
+    if((dataAtual.mes <= mesNascimento_)&&(dataAtual.dia < diaNascimento_)){
+        idade --;
+    }
+    console.log(idade)
     return idade;
 }
 
@@ -105,6 +125,76 @@ function classificarIdade(idade_){
 }
 
 // Passo 4
+function organizarDados(dadosUsuario_, valorIdade_, classificacaoIdade_){
+    //Organizando o objeto para salvar
+    let dadosUsuarioAtualizado = {
+        ...dadosUsuario_,
+        idade: valorIdade_,
+        faixa: classificacaoIdade_
+    }
+
+    return dadosUsuarioAtualizado;
+}
+
+// Passo 5
+function cadastrarUsuario(dadosUsuario_){
+    let listaUsuarios = [];
+    // localStorage.setItem("User", "Cleber")
+
+    //Se tem usuário cadastrado
+    if(localStorage.getItem("usuariosCadastrados") != null){
+        //Guarda as informações na Array e converte string para Objeto
+        listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+    }
+    //Se não houver usuário cadastrar o atual
+    listaUsuarios.push(dadosUsuario_);
+    //Salva a lista de usuarios e converte de Objeto para String
+    localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuarios));
+}
+
+//Passo 6
+function carregarUsuarios(){
+    let listaCarregada = [];
+    if(localStorage.getItem("usuariosCadastrados") != null){
+        //Guarda as informações na Array e converte string para Objeto
+        listaCarregada = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+    }
+
+    if(listaCarregada.length == 0){
+        //se a lista estiver vazia mostre uma msg
+        let tabela = document.getElementById("corpo-tabela");
+
+        tabela.innerHTML = `<tr class="linha-mensagem">
+            <td colspan="6">Nenhum usuário cadastrado 😢</td>
+        </tr>`
+    } else{
+        //Mostrar conteúdo da tabela
+        montarTabela(listaCarregada);
+    }
+
+    console.log (listaCarregada);
+}
+window.addEventListener("DOMContentLoaded", () => carregarUsuarios());
+
+function montarTabela(listaUsuarios_){
+    let tabela = document.getElementById("corpo-tabela");
+
+    let template = "";
+
+    listaUsuarios_.forEach(usuario => {
+        template += `<tr>
+            <td data-cell="nome">${usuario.nome}</td>
+            <td data-cell="data de nascimento">${usuario.dia}/${usuario.mes}/${usuario.ano}</td>
+            <td data-cell="idade">${usuario.idade} anos</td>
+             <td data-cell="faixa etária">${usuario.faixa}</td>
+        </tr>`
+        
+    });
+    tabela.innerHTML = template;
+}
 
 
-
+function deletarRegistros(){
+    localStorage.removeItem("usuariosCadastrados");
+    window.location.reload();
+}
